69a2ba9f39d42e0cb2da39744c6060b8eb57039d
A-NGJ <anagaj99@gmail.com>
2020-11-24 19:22:07 +0100

    merged with premeek changes

--- bee_graduater.py
+++ bee_graduater.py
@@ -34,8 +34,7 @@ class BaseGraduaterBee(ABC):
             pos[pos < self.minf] = self.minf
         if pos[1] > self.maxl:
             pos[1] = self.maxl
-        maxw = min(2*pos[2], self.obj_function.free_time(pos))
-        if maxw > pos[3]:
+        if maxw:= min(2*pos[2], self.obj_function.free_time(pos))> pos[3]:
             pos[3] = maxw
         return pos
 
