54ba5d1111bbbc2b6106bc195de56fb829c0e7a9
A-NGJ <anagaj99@gmail.com>
2020-11-24 01:34:51 +0100

    added objective base function

--- objective.py
+++ objective.py
@@ -1,6 +1,28 @@
 import numpy as np
 
+from abc import ABCMeta, abstractmethod
+from six import add_metaclass
 
+@add_metaclass(ABCMeta)
+class ObjectiveFunction(object):
+
+    def __init__(self, name, dim, minf, maxf):
+        self.name = name
+        self.dim = dim
+        self.minf = minf
+        self.maxf = maxf
+
+    def sample(self):
+        return np.random.uniform(low=self.minf, high=self.maxf, size=self.dim)
+
+    def custom_sample(self):
+        return np.repeat(self.minf, repeats=self.dim) \
+            + np.random.uniform(low=0, high=1, size=self.dim) *\
+            np.repeat(self.maxf-self.minf, repeats=self.dim)
+
+    @abstractmethod
+    def evaluate(self, x):
+        pass
 
 class Objective:
     coeB = 'jakas liczba'
@@ -14,3 +36,4 @@ class Objective:
 
     def objective_function(self):
         return (self.coeB*self.AVG + self.Tw + self.coeA*self.P)
+
--- term_graduater.py
+++ term_graduater.py
@@ -3,7 +3,9 @@ import random
 
 class Graduater:
 
-    def __init__(self):
+    def __init__(self, offset: int):
+        self.offset = offset
+
         self.T_n = Food(60, 0)
         self.T_s = Food(9, 0)
         self.T_w = Food(60, 0)
@@ -13,7 +15,9 @@ class Graduater:
         self.I = Food(min(self.T_w.x, 2*self.P.x), 0)
         self.I.x = random.randint(self.I.l_i, self.I.u_i)
 
+    def _
+
 if __name__ == "__main__":
 
-    graduater = Graduater()
+    graduater = Graduater(3)
     print("")
